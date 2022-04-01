7f18fcde3adcb8d346fcad419ac25ab3bf66a1e7
Lucas Baltar Esnaty Boudoux <lucasesnaty@gmail.com>
2022-04-01 16:25:35 -0300

    Exercícios Java

--- Atividades Programação Sequencial/Exercicio4.java
+++ Atividades Programação Sequencial/Exercicio4.java
@@ -20,27 +20,27 @@ public class Exercicio4 {
 		System.out.println("\nTerceiro número: ");
 		c = leia.nextFloat();
 		
-		if (a>b && a>c && b>c)
+		if (a>=b && a>=c && b>=c)
 		{
 			System.out.println("A ordem é: "+c+", "+b+", "+a);
 		}
-		else if (a>b && a>c && c>b)
+		else if (a>=b && a>=c && c>=b)
 		{
 			System.out.println("A ordem é: "+b+", "+c+", "+a);
 		}
-		else if (b>a && b>c && a>c)
+		else if (b>=a && b>=c && a>=c)
 		{
 			System.out.println("A ordem é: "+c+", "+a+", "+b);
 		}
-		else if (b>a && b>c && c>a)
+		else if (b>=a && b>=c && c>=a)
 		{
 			System.out.println("A ordem é: "+a+", "+c+", "+b);
 		}
-		else if (c>a && c>b && a>b)
+		else if (c>=a && c>=b && a>=b)
 		{
 			System.out.println("A ordem é: "+b+", "+a+", "+c);
 		}
-		else if (c>a && c>b && b>a)
+		else if (c>=a && c>=b && b>=a)
 		{
 			System.out.println("A ordem é: "+a+", "+b+", "+c);
 		}
